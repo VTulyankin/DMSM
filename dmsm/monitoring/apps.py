975f0432b9dc218eeaf1c12aa2cedd6b62ba5477
VTulyankin <Vladimir.Tuliankin@urfu.me>
2026-06-13 02:48:00 +0500

    Connect fetcher and db
    Late night coding, 100% free of bugs

--- dmsm/monitoring/apps.py
+++ dmsm/monitoring/apps.py
@@ -1,6 +1,15 @@
+import os
+import threading
 from django.apps import AppConfig
-
 
 class MonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dmsm.monitoring'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN'):
+            from dmsm.fetcher import ServerFetcher
+            
+            fetcher = ServerFetcher()
+            thread = threading.Thread(target=fetcher.start_loop, daemon=True)
+            thread.start()
--- dmsm/rcon_client.py
+++ dmsm/rcon_client.py
@@ -1,6 +1,5 @@
 import socket
 import struct
-import time
 from django.conf import settings
 
 class MinecraftRCONClient:
@@ -8,29 +7,21 @@ class MinecraftRCONClient:
         self.host = host or getattr(settings, 'RCON_HOST')
         self.port = port or getattr(settings, 'RCON_PORT')
         self.password = password or getattr(settings, 'RCON_PASSWORD')
-        self.interval = getattr(settings, 'FETCH_INTERVAL', 60)
         self.sock = None
 
     def connect(self):
-        while True:
-            try:
-                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.settimeout(5.0)
-                self.sock.connect((self.host, int(self.port)))
-                
-                packet = struct.pack('<ii', 1, 3) + self.password.encode('utf-8') + b'\x00\x00'
-                self.sock.sendall(struct.pack('<i', len(packet)) + packet)
-                response_length_data = self.sock.recv(4)
-                
-                if response_length_data:
-                    response_length = struct.unpack('<i', response_length_data)[0]
-                    self.sock.recv(response_length)
-                
-                break
-            except Exception:
-                print("offline")
-                self.disconnect()
-                time.sleep(self.interval)
+        self.disconnect()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(5.0)
+        self.sock.connect((self.host, int(self.port)))
+        
+        packet = struct.pack('<ii', 1, 3) + self.password.encode('utf-8') + b'\x00\x00'
+        self.sock.sendall(struct.pack('<i', len(packet)) + packet)
+        response_length_data = self.sock.recv(4)
+        
+        if response_length_data:
+            response_length = struct.unpack('<i', response_length_data)[0]
+            self.sock.recv(response_length)
 
     def disconnect(self):
         if self.sock:
@@ -41,29 +32,29 @@ class MinecraftRCONClient:
             self.sock = None
 
     def command(self, cmd, packet_type=2):
-        while True:
-            if not self.sock:
-                self.connect()
-            try:
-                packet = struct.pack('<ii', 1, packet_type) + cmd.encode('utf-8') + b'\x00\x00'
-                self.sock.sendall(struct.pack('<i', len(packet)) + packet)
-                response_length_data = self.sock.recv(4)
+        if not self.sock:
+            self.connect()
+        try:
+            packet = struct.pack('<ii', 1, packet_type) + cmd.encode('utf-8') + b'\x00\x00'
+            self.sock.sendall(struct.pack('<i', len(packet)) + packet)
+            response_length_data = self.sock.recv(4)
+            
+            if not response_length_data:
+                self.disconnect()
+                raise ConnectionError("Empty response from server")
                 
-                if not response_length_data:
-                    self.connect()
-                    continue
-                    
-                response_length = struct.unpack('<i', response_length_data)[0]
-                response_data = b""
+            response_length = struct.unpack('<i', response_length_data)[0]
+            response_data = b""
+            
+            while len(response_data) < response_length:
+                chunk = self.sock.recv(response_length - len(response_data))
+                if not chunk:
+                    break
+                response_data += chunk
                 
-                while len(response_data) < response_length:
-                    chunk = self.sock.recv(response_length - len(response_data))
-                    if not chunk:
-                        break
-                    response_data += chunk
-                    
-                if len(response_data) >= 8:
-                    return response_data[8:-2].decode('utf-8', errors='ignore')
-                return ""
-            except Exception:
-                self.connect()
+            if len(response_data) >= 8:
+                return response_data[8:-2].decode('utf-8', errors='ignore')
+            return ""
+        except Exception:
+            self.disconnect()
+            raise
